7406e1faae371d85621c8d8481b898e49b610392
Dastagiri13 <dastagiridudekula999@gmail.com>
2020-10-21 00:27:42 +0530

    Updates

---
+++ precache-manifest.9720bcfc85846e3a839a8cd2430e2400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2930b80fd7ab9bd53eea92a3a51ff2",
+    "url": "/master-rolodex1/index.html"
+  },
+  {
+    "revision": "6a32b800ceb2acdc2e69",
+    "url": "/master-rolodex1/static/css/main.cde70bab.chunk.css"
+  },
+  {
+    "revision": "ca316142891b6c9554cc",
+    "url": "/master-rolodex1/static/js/2.62ae158c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/master-rolodex1/static/js/2.62ae158c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a32b800ceb2acdc2e69",
+    "url": "/master-rolodex1/static/js/main.fd179e88.chunk.js"
+  },
+  {
+    "revision": "70eb5d9f3174c998cbcd",
+    "url": "/master-rolodex1/static/js/runtime-main.e5b0cfef.js"
+  }
+]);
